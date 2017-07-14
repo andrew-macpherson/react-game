8e2cf0a4b4f23f84fde7b2011cf36f4d201db7e7
Andrew MacPherson <andrewmac17@gmail.com>
2017-07-13 21:39:55 -0400

    before react game kit

--- src/index.js
+++ src/index.js
@@ -3,20 +3,73 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Map extends React.Component{
-	render(){
+class Board extends React.Component{
 
+	renderPlayer(){
+		return <Player />;
+	}
+
+	render(){
 		return (
-			<div>Map</div>
+			<div>
+			{this.renderPlayer()}
+			</div>
+		);
+	}
+}
+
+class Player extends React.Component{
+
+	constructor(){
+		super();
+
+		this.state = {
+			top: 0,
+			left: 0
+		}
+	}
+
+
+
+	handleKeyDown = (e) => {
+		switch(e.keyCode){
+			case 37:
+				console.log('left');
+				//this.position.left = this.position.left - 1;
+				break;
+			case 38:
+				console.log('top');
+				//this.position.top = this.position.top - 1;
+				break;
+			case 39:
+				console.log('right');
+				//this.position.left = this.position.left + 1;
+				break;
+			case 40: 
+				console.log('down');
+				//this.position.top = this.position.top + 1;
+				break;
+		}
+	}
+
+	render(){
+		return (
+			<div className="player" style={this.props}></div>
 		)
+	}
+
+	componentDidMount(){
+		window.onkeydown = this.handleKeyDown;
 	}
 }
 
 class Game extends React.Component{
 	render(){
 		return (
-			<Map />
-		)
+			<div className="board">
+				<Board />
+			</div>
+		);
 	}
 }
 
